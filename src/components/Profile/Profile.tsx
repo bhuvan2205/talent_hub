33756d16fd662ad36a84d7d216af5ab2c631b812
bhuvan2205 <bhuvancs2205@gmail.com>
2024-05-25 23:46:57 +0530

    Omit: Unnecessary packages

--- src/components/Profile/Profile.tsx
+++ src/components/Profile/Profile.tsx
@@ -12,39 +12,42 @@ import Link from "next/link";
 import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 
 export default async function Profile() {
-	const { getUser } = getKindeServerSession();
+	const { getUser, isAuthenticated } = getKindeServerSession();
+	const isLoggedIn = await isAuthenticated();
 	const user = await getUser();
 
 	return (
 		<>
-			<Popover>
-				<PopoverTrigger asChild>
-					<Avatar className="cursor-pointer">
-						<AvatarImage
-							src="https://github.com/shadcn.png"
-							alt={user?.given_name as string}
-						/>
-						<AvatarFallback>
-							{user?.given_name?.at(0)?.toUpperCase()}
-						</AvatarFallback>
-					</Avatar>
-				</PopoverTrigger>
-				<PopoverContent className="w-fit px-6">
-					<div>
-						<Link
-							href={ROUTES.DASBOARD}
-							className="py-2 flex items-center gap-2">
-							<User2 />
-							<span>{user?.given_name}</span>
-						</Link>
-					</div>
-					<div className="py-2">
-						<Badge className="cursor-pointer w-full">
-							<LogoutLink className="text-center w-full">Log out</LogoutLink>
-						</Badge>
-					</div>
-				</PopoverContent>
-			</Popover>
+			{isLoggedIn && (
+				<Popover>
+					<PopoverTrigger asChild>
+						<Avatar className="cursor-pointer">
+							<AvatarImage
+								src={user?.picture as string}
+								alt={user?.given_name as string}
+							/>
+							<AvatarFallback>
+								{user?.given_name?.at(0)?.toUpperCase()}
+							</AvatarFallback>
+						</Avatar>
+					</PopoverTrigger>
+					<PopoverContent className="w-fit px-6">
+						<div>
+							<Link
+								href={ROUTES.DASBOARD}
+								className="py-2 flex items-center gap-2">
+								<User2 />
+								<span>{user?.given_name}</span>
+							</Link>
+						</div>
+						<div className="py-2">
+							<Badge className="cursor-pointer w-full">
+								<LogoutLink className="text-center w-full">Log out</LogoutLink>
+							</Badge>
+						</div>
+					</PopoverContent>
+				</Popover>
+			)}
 		</>
 	);
 }
